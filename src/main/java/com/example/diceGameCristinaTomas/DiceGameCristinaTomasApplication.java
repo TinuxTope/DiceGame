957e0a79d301c067366f3a0869953a3197c5ef56
TinuxTope <c.tomaspe@gmail.com>
2024-07-10 20:10:21 +0200

    first commit

---
+++ src/main/java/com/example/diceGameCristinaTomas/DiceGameCristinaTomasApplication.java
@@ -0,0 +1,13 @@
+package com.example.diceGameCristinaTomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceGameCristinaTomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiceGameCristinaTomasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diceGameCristinaTomas/DiceGameCristinaTomasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diceGameCristinaTomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceGameCristinaTomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
